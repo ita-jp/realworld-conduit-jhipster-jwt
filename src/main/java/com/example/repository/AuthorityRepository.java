3f5dbd2a47d7fa2db64c95ce7ae7d8ee54280889
ita-jp <28795978+ita-jp@users.noreply.github.com>
2024-04-30 20:38:25 +0900

    Initial version of conduit generated by generator-jhipster@8.4.0

---
+++ src/main/java/com/example/repository/AuthorityRepository.java
@@ -0,0 +1,12 @@
+package com.example.repository;
+
+import com.example.domain.Authority;
+import org.springframework.data.jpa.repository.*;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Spring Data JPA repository for the Authority entity.
+ */
+@SuppressWarnings("unused")
+@Repository
+public interface AuthorityRepository extends JpaRepository<Authority, String> {}
